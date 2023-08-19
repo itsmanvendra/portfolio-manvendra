24c607e28ca01edef647e48ee838ca4fad1cc248
itsmanvendra <singh03manvendra@gmail.com>
2023-08-19 22:12:44 +0530

    first phase done

---
+++ src/assets/index.js
@@ -0,0 +1,80 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import Manvendra from "./Manvendra.jpg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import firebase from "./tech/firebase.png";
+import materialui from "./tech/materialui.png";
+import bootstrap from "./tech/bootstrap.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import beanbyte from "./company/beanbyteSoftware.png";
+import freelancer from "./company/freelancer.jpg";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import invoiceapp from "./invoiceapp.png";
+import ecommerce from "./ecommerce.png";
+import aaieg from "./aaieg.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  Manvendra,
+  firebase,
+  materialui,
+  bootstrap,
+  beanbyte,
+  freelancer,
+  invoiceapp,
+  ecommerce,
+  aaieg,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,232 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  firebase,
+  materialui,
+  bootstrap,
+  beanbyte,
+  freelancer,
+  invoiceapp,
+  ecommerce,
+  aaieg,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "ReactJS Developer",
+    icon: mobile,
+  },
+  
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "Material UI",
+    icon: materialui,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "firebase",
+    icon: firebase,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Beanbyte Software",
+    icon: beanbyte,
+    iconBg: "#E6DEDD",
+    date: "May 2023 - Current",
+    points: [
+      "Developed single-page web applications Customer Relationship Management(CRM) using React.js and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Implemented performance optimizations, including code splitting and lazy loading techniques, resulting in significantly reduced initial load times and an overall improved user experience.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    ], 
+  },
+  {
+    title: "ReactJs Developer",
+    company_name: "Freelancer",
+    icon: freelancer,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Apr 2023",
+    points: [
+      "Designed and developed a single-page web app for the AAIEG using React.js and other related technologies.",
+      "Integrated JSON Web Tokens (JWT) to handle both authentication and authorization. Provide role-based access control on the application, enhancing data protection and user privacy.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Collaborated seamlessly with a cross-functional team of 3 members to successfully deliver the project.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "As a fresher, Manvendra's eagerness to learn and adapt in a fast-paced environment has been remarkable.",
+    name: "Lokesh Kumar",
+    designation: "CEO",
+    company: "Beanbyte Software",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
+  },
+  {
+    testimonial:
+      "Manvendra's dedication to delivering pixel-perfect designs and their enthusiasm for front-end technologies make them a valuable addition to any team.",
+    name: "Kunal Goyal",
+    designation: "Frontend Developer",
+    company: "Beanbyte Software",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "I appreciated Manvendra's fresh ideas and their ability to bring them to life on the frontend.",
+    name: "Sushil Kumar",
+    designation: "CEO",
+    company: "Tanil Solutions",
+    image: "https://randomuser.me/api/portraits/men/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "AAIEG",
+    description:
+      "Designed and developed an internal portal for AAIEG, facilitating seamless communication and efficient information sharing among team members.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: aaieg,
+    source_code_link: "https://aaieg.com/",
+  },
+  {
+    name: "Ecommerce Website",
+    description:
+      "Developed e-commerce with Google authenticated security and an intuitive cart/wishlist interface, delivering seamless item management for better experience.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ecommerce,
+    source_code_link: "https://github.com/itsmanvendra/nikeStore",
+  },
+  {
+    name: "Invoice App",
+    description:
+      "Created an invoice app with user-friendly features and strong security. Improved user experience through smart design and secure authentication methods.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: invoiceapp,
+    source_code_link: "https://github.com/itsmanvendra/invoice-app",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
